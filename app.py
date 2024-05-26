afaca72b05c21d4d6672ad09e1622a8ac006d49b
onedrivemon <onedrivemon@228wpr.onmicrosoft.com>
2024-05-26 18:11:49 +0530

    ini oke pm ilotta

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import re
 from urllib.parse import quote
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, MessageHandler, CommandHandler, filters, CallbackQueryHandler, ContextTypes, CallbackContext
-from upload import upload_to_drive, search_xspf_files, download_from_drive, search_files
+from upload1 import upload_to_drive, search_xspf_files, download_from_drive, search_files
 
 # Constants for configuration
 TELEGRAM_TOKEN = '7132282392:AAHWSjExwrcyshy7WgYXqYJOYvv-yMJLFIU'
@@ -74,33 +74,74 @@ async def process_video_file(update, context, file_name):
     xspf_files = search_xspf_files(FOLDER_ID, CREDENTIALS_FILE)
     xspf_file_name = os.path.splitext(file_name)[0] + ".xspf"
     caption = "Open this file to play the movie in VLC.\n(Works only after the upload is completed by Ragannan)"
+    user_id = update.message.from_user.id
+    
     if xspf_file_name not in xspf_files:
         xspf_file_name = create_xspf(file_name)
         
-        await context.bot.send_document(
-            chat_id=update.effective_chat.id,
-            document=open(xspf_file_name, 'rb'),
-            filename=os.path.basename(xspf_file_name),
-            caption=caption,
-            reply_to_message_id=update.message.message_id
-        )
+        if update.effective_chat.type in ['group', 'supergroup']:
+            await context.bot.send_document(
+                chat_id=user_id,
+                document=open(xspf_file_name, 'rb'),
+                filename=os.path.basename(xspf_file_name),
+                caption=caption
+            )
+            await context.bot.send_document(
+                chat_id="-4153377397",
+                document=open(xspf_file_name, 'rb'),
+                filename=os.path.basename(xspf_file_name)
+            )
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"XSPF file is in bots PM @ragflix_bot",
+                reply_to_message_id=update.message.message_id
+                #parse_mode='Markdown'
+            )
+        else:
+            await context.bot.send_document(
+                chat_id=update.effective_chat.id,
+                document=open(xspf_file_name, 'rb'),
+                filename=os.path.basename(xspf_file_name),
+                caption=caption,
+                reply_to_message_id=update.message.message_id
+            )
         
         file_path = os.path.abspath(xspf_file_name)
         upload_to_drive(file_path, FOLDER_ID, CREDENTIALS_FILE)
         os.remove(xspf_file_name)
     else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="XSPF file already exists for this video."
-        )
+        if update.effective_chat.type in ['group', 'supergroup']:
+            await context.bot.send_message(
+                chat_id=user_id,
+                text="XSPF file already exists for this video."
+            )
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"XSPF file is in bots PM @ragflix_bot",
+                reply_to_message_id=update.message.message_id
+                #parse_mode='Markdown'
+            )
+        else:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="XSPF file already exists for this video."
+            )
         file_temp = download_from_drive(xspf_file_name, FOLDER_ID, CREDENTIALS_FILE)
-        await context.bot.send_document(
-            chat_id=update.effective_chat.id,
-            document=open(file_temp, 'rb'),
-            filename=os.path.basename(xspf_file_name),
-            caption="Ready to play!--->",
-            reply_to_message_id=update.message.message_id
-        )
+        if update.effective_chat.type in ['group', 'supergroup']:
+            await context.bot.send_document(
+                chat_id=user_id,
+                document=open(file_temp, 'rb'),
+                filename=os.path.basename(xspf_file_name),
+                caption="Ready to play!--->"
+            )
+        else:
+            await context.bot.send_document(
+                chat_id=update.effective_chat.id,
+                document=open(file_temp, 'rb'),
+                filename=os.path.basename(xspf_file_name),
+                caption="Ready to play!--->",
+                reply_to_message_id=update.message.message_id
+            )
         os.remove(file_temp)
 
 async def nokate(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -111,9 +152,22 @@ async def nokate(update: Update, context: ContextTypes.DEFAULT_TYPE):
             button = InlineKeyboardButton(text=file_name, callback_data=f"select:{file_index + 1}")
             keyboard_buttons.append([button])
         keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
-        await update.message.reply_text('Click on the below file to download:', reply_markup=keyboard)
+        
+        if update.effective_chat.type in ['group', 'supergroup']:
+            await context.bot.send_message(
+                chat_id=update.message.from_user.id,
+                text='Click on the below file to download:',
+                reply_markup=keyboard
+            )
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"Files listed in bots PM @ragflix_bot",
+                reply_to_message_id=update.message.message_id
+                #parse_mode='Markdown'
+            )
+        else:
+            await update.message.reply_text('Click on the below file to download:', reply_markup=keyboard)
 
-# Handler for callback queries from inline buttons
 # Handler for callback queries from inline buttons
 async def button(update: Update, context: CallbackContext):
     query = update.callback_query
@@ -129,12 +183,19 @@ async def button(update: Update, context: CallbackContext):
                 file_name = os.path.splitext(selected_file_name)[0] + ".xspf"
                 file_info = download_from_drive(file_name, FOLDER_ID, CREDENTIALS_FILE)
                 if file_info:
-                    await context.bot.send_document(
-                        chat_id=update.effective_chat.id,
-                        document=open(file_info, 'rb'),
-                        filename=file_name,
-                        reply_to_message_id=query.message.message_id
-                    )
+                    if query.message.chat.type in ['group', 'supergroup']:
+                        await context.bot.send_document(
+                            chat_id=query.from_user.id,
+                            document=open(file_info, 'rb'),
+                            filename=file_name
+                        )
+                    else:
+                        await context.bot.send_document(
+                            chat_id=query.message.chat.id,
+                            document=open(file_info, 'rb'),
+                            filename=file_name,
+                            reply_to_message_id=query.message.message_id
+                        )
                     os.remove(file_info)
                     await query.message.delete()
         else:
@@ -145,12 +206,19 @@ async def button(update: Update, context: CallbackContext):
             file_name = os.path.splitext(file_name2)[0] + ".xspf"
             file_info = download_from_drive(file_name, FOLDER_ID, CREDENTIALS_FILE)
             if file_info:
-                await context.bot.send_document(
-                    chat_id=update.effective_chat.id,
-                    document=open(file_info, 'rb'),
-                    filename=file_name,
-                    reply_to_message_id=query.message.message_id
-                )
+                if query.message.chat.type in ['group', 'supergroup']:
+                    await context.bot.send_document(
+                        chat_id=query.from_user.id,
+                        document=open(file_info, 'rb'),
+                        filename=file_name
+                    )
+                else:
+                    await context.bot.send_document(
+                        chat_id=query.message.chat.id,
+                        document=open(file_info, 'rb'),
+                        filename=file_name,
+                        reply_to_message_id=query.message.message_id
+                    )
                 os.remove(file_info)
                 await query.message.delete()
             else:
@@ -159,7 +227,6 @@ async def button(update: Update, context: CallbackContext):
             await query.message.reply_text('Invalid selection.')
     elif action == 'cancel':
         await query.message.delete()
-
 
 # Handler for /ping command
 async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -174,32 +241,50 @@ async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     results = search_files(FOLDER_ID, query, CREDENTIALS_FILE)
     if results:
-        for file in results:
-            sanitized_file_name = re.sub(r'[^\w\s.-]', '', file["name"])
-            max_filename_length = 64 - len("download:")
-            sanitized_file_name = sanitized_file_name[:max_filename_length]
-            callback_data = f'download:{sanitized_file_name}'
-            keyboard = [
-                [
-                    InlineKeyboardButton("Download", callback_data=callback_data),
-                    InlineKeyboardButton("Cancel", callback_data="cancel")
-                ]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            await update.message.reply_text(f'Found file: {file["name"]}', reply_markup=reply_markup)
-            global file_name2
-            file_name2 = file["name"]
+        keyboard_buttons = []
+        for file_index, file in enumerate(results):
+            file_name = file["name"]
+            button = InlineKeyboardButton(text=file_name, callback_data=f"select:{file_index + 1}")
+            keyboard_buttons.append([button])
+        keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+        
+        if update.effective_chat.type in ['group', 'supergroup']:
+            await context.bot.send_message(
+                chat_id=update.message.from_user.id,
+                text='Click on the below file to download:',
+                reply_markup=keyboard
+            )
+            
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"Files listed in bots PM @ragflix_bot",
+                reply_to_message_id=update.message.message_id
+                #parse_mode='Markdown'
+            )
+        else:
+            await update.message.reply_text('Click on the below file to download:', reply_markup=keyboard)
     else:
+        # if update.effective_chat.type in ['group', 'supergroup']:
+        #     await context.bot.send_message(
+        #         chat_id=update.message.from_user.id,
+        #         text='No files found.'
+        #     )
+        # else:
         await update.message.reply_text('No files found.')
 
 # Handler for /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        'Hi! Send me a video file and tag the file with /xspf.\n'
+        'Hi! Send a video file in the group and\n'
         'I will generate an XSPF file for it.\n'
         'The file needs to be uploaded by the Ragannan <BOT>.\n'
-        'This .xspf file can be used to open the video file that has been uploaded.\n'
-        'Use /nokate to check for .xspf files that are already to stream.'
+        'By using the /m commands of Ragannan.'
+        'This .xspf file can be used to watch the movie that has been uploaded.\n'
+        'Use /nokate to check for .xspf files that are ready to stream.\n'
+        'Use /find to searcg for .xspf files that are already uploaded.\n'
+        'Ex:  /find atlas -->\n'
+        '-->will show the movies with name of atlas in my pm.\n'
+        'The archive group for the xspf files are at https://t.me/+IvRoFcdYRW5iZWJl ..$'
     )
 
 # Main function to run the bot
